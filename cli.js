aa86b55315ce422ffb05c3ee4893e82d608c9d05
CodeDotJS <rushforlinux@gmail.com>
2016-07-11 16:15:15 +0530

    up and clean

---
+++ cli.js
@@ -0,0 +1,45 @@
+#!/usr/bin/env node
+'use strict';
+
+const stafo = require('stafo');
+const chalk = require('chalk');
+const ora = require('ora');
+const logUpdate = require('log-update');
+const updateNotifier = require('update-notifier');
+const pkg = require('./package.json');
+
+updateNotifier({pkg}).notify();
+
+const spinner = ora();
+
+const arg = process.argv[2];
+const pre = `${chalk.bold.cyan('›')} `;
+
+if (!arg || arg === '-h') {
+	console.log(`
+ ${chalk.cyan('Usage   :')} stafo <${chalk.bold('username/repository')}>
+
+ ${chalk.cyan('Example :')} stafo CodeDotJS/kote   ${chalk.dim('[user]')}
+ 	   stafo facebook/reactjs ${chalk.dim('[organization]')}
+	`);
+}
+
+if (arg) {
+	logUpdate();
+	spinner.start();
+	spinner.text = chalk.dim(`Fetching stars and forks on ${chalk.bold(arg)}`);
+	stafo.repo(arg).then(user => {
+		const inf = [];
+		const repoCount = (prefix, key) => {
+			if (user[key]) {
+				inf.push(`${prefix} : ${user[key]}`);
+			}
+		};
+		logUpdate();
+		repoCount(`${pre} Stars`, 'star');
+		repoCount(`${pre} Forks`, 'fork');
+		console.log(inf.join('\n'));
+		console.log();
+		spinner.stop();
+	});
+}
---
+++ test.js
@@ -0,0 +1,23 @@
+import childProcess from 'child_process';
+import test from 'ava';
+
+test.cb('main', t => {
+	const cp = childProcess.spawn('./cli.js', {stdio: 'inherit'});
+
+	cp.on('error', t.ifError);
+
+	cp.on('close', code => {
+		t.is(code, 0);
+		t.end();
+	});
+});
+
+test.cb('stafo', t => {
+	childProcess.execFile('./cli.js', ['CodeDotJS/kote'], {
+		cwd: __dirname
+	}, (err, stdout) => {
+		t.ifError(err);
+		t.true(stdout === '\u001b[?25l\n\u001b[?25l\u001b[1000D\u001b[K\u001b[1A\u001b[1000D\u001b[K\n›  Stars : 1\n›  Forks : 0\n\n\u001b[?25h');
+		t.end();
+	});
+});
